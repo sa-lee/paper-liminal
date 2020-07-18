4b60ea2c2a7774860e60102acabbf55ebcc36ee9
Stuart Lee <lee.s@wehi.edu.au>
2020-07-18 12:31:37 +1000

    adjust figures and section

--- scripts/liminal-trees.R
+++ scripts/liminal-trees.R
@@ -1,6 +1,13 @@
 ## ----init-----------------------------------------------------------
 library(liminal)
 dim(fake_trees)
+library(cowplot)
+library(ggplot2)
+
+tree_img <- ggdraw() + 
+  draw_image(here::here("img", "tree.png"), scale = 0.8) +
+  draw_figure_label("Tree Structure", position = "bottom.right") +
+  theme_void()
 
 
 ## ----pc-view--------------------------------------------------------
@@ -10,8 +17,13 @@ summary(pcs)$importance[3,1:12]
 
 ## ----pca-xy---------------------------------------------------------
 fake_trees <- dplyr::bind_cols(fake_trees, as.data.frame(pcs$x))
-limn_xy(fake_trees, x = PC1, y = PC2, color = branches)
 
+pca_xy <- ggplot(fake_trees, aes(x = PC1, y = PC2, color = branches)) +
+  geom_point() +
+  scale_color_manual(values = limn_pal_tableau10()) +
+  labs(caption = "PCA") +
+  theme(aspect.ratio = 1) +
+  theme_void() 
 
 ## ----tsne-xy--------------------------------------------------------
 set.seed(2099)
@@ -21,14 +33,22 @@ tsne_df <- data.frame(tsneX = tsne$Y[,1],
                       tsneY = tsne$Y[,2],
                       branches = fake_trees$branches)
 
-limn_xy(tsne_df, x = tsneX, y = tsneY, color = branches)
+tsne_xy  <- ggplot(tsne_df, aes(x = tsneX, y = tsneY, color = branches)) +
+  geom_point() +
+  scale_color_manual(values = limn_pal_tableau10()) + 
+  guides(color = FALSE) +
+  labs(caption = "tSNE") +
+  theme(aspect.ratio = 1) +
+  theme_void() 
+
+## ----tour----------------------------------------------------------
 
 # now together
-limn_tour_xylink(
-  dplyr::select(fake_trees, PC1:PC12, branches),
-  tsne_df,
-  x_color = branches,
-  y_color = branches
+limn_tour_link(
+  tour_data = dplyr::select(fake_trees, PC1:PC12, branches),
+  embed_data = tsne_df,
+  embed_color = branches,
+  tour_color = branches
 )
 
 
@@ -40,16 +60,40 @@ tsne_v2 <- Rtsne::Rtsne(
   perplexity = 100
 )
 
+
+
 tsne_v2_df <- data.frame(
   tsneX = tsne_v2$Y[,1], 
   tsneY = tsne_v2$Y[,2], 
   branches = fake_trees$branches
 )
 
-limn_tour_xylink(
-  dplyr::select(fake_trees, PC1:PC12, branches),
-  tsne_v2_df,
-  x_color = branches,
-  y_color = branches
+tsne_xy_v2  <- ggplot(tsne_v2_df, aes(x = tsneX, y = tsneY, color = branches)) +
+  geom_point() +
+  scale_color_manual(values = limn_pal_tableau10()) + 
+  guides(color = FALSE) +
+  labs(caption = "tSNE (PCA initialised)") +
+  theme(aspect.ratio = 1) +
+  theme_void() 
+
+
+clean_fig <- plot_grid(
+  tree_img, 
+  pca_xy,
+  tsne_xy,
+  tsne_xy_v2,
+  ncol = 2, 
+  labels = "auto"
+)
+
+ggsave2(here::here("img", "tree-view.png"), plot = clean_fig)
+
+## --arrange---------
+
+limn_tour_link(
+  tour_data = dplyr::select(fake_trees, PC1:PC12, branches),
+  embed_data = tsne_v2_df,
+  tour_color = branches,
+  embed_color = branches
 )
 
