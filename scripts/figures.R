856380da04fb972330228ef4b990d85cdaa4ebf1
Stuart Lee <lee.s@wehi.edu.au>
2020-08-03 16:40:54 +1000

    redo screenshots

--- scripts/figures.R
+++ scripts/figures.R
@@ -2,40 +2,58 @@
 library(ggplot2)
 library(cowplot)
 
-draw_screenshot <- function(file) {
+draw_screenshot <- function(file, scale = 1) {
   ggdraw() + 
-    draw_image(here::here("img", file), scale = 0.8) +
-    theme_void()
+    draw_image(here::here("img", file), scale = scale)
 }
 
 enframe_screenshot <- function(lst) {
   plot_grid(plotlist = lst, 
-            ncol = 2, 
+            nrow = 2, 
             labels = "auto",
             align = "hv",
-            axis = "t" )
+            axis = "tblr" )
 }
 
-case_fig <- function(case) {
+case_fig <- function(case, scale = 1) {
   files <- list.files(here::here("img"), 
                       pattern = ".png$")
   
   files <- files[grep(paste0("^", case), files)]
 
-  enframe_screenshot(lapply(files, draw_screenshot))
+  enframe_screenshot(lapply(files, draw_screenshot, scale = scale))
 }
 
 cases <- c("gaussian", 
            "hierarchical", 
            "trees-01", 
            "trees-02", 
-           "mouse-01",
            "mouse-02")
 
 for (case in cases) {
   fout <- paste0("liminal-screenshot-", case, ".png")
   pout <- case_fig(case)
-  
-  ggsave2(here::here("img", fout), plot = pout)
-  
+  ggsave2(here::here("img", fout), plot = pout,
+          height = 6, 
+          width = 4)
 }
+
+# special case for `mouse-01`
+
+mpout <- function() {
+  files <- list.files(here::here("img"), 
+                      pattern = ".png$")
+  
+  files <- files[grep(paste0("^", "mouse-01"), files)]
+  lapply(files, draw_screenshot, scale = 1)
+}
+
+pout <- plot_grid(plotlist = mpout(),
+          nrow = 4, align = "hv", axis = "tblr", 
+          labels = "auto")
+
+ggsave2(here::here("img", "liminal-screenshot-mouse-01.png"),
+        plot = pout,
+        height = 8,
+        width = 4)
+
